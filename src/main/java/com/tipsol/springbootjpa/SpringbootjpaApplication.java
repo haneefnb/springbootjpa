6b50e38fb04bb5de87378e22f0137ea6a20e990d
1000475 <1000475@WT-5CD72369KQ.mshome.net>
2021-11-21 17:31:52 +0530

    after project addition

--- src/main/java/com/tipsol/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/com/tipsol/springbootjpa/SpringbootjpaApplication.java
@@ -1,13 +1,23 @@
 package com.tipsol.springbootjpa;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringbootjpaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootjpaApplication.class, args);
+		System.out.println("----Started------");
+	}
+	
+	@Bean
+	public ModelMapper getModelMapper() {
+		return new ModelMapper();
 	}
 
 }
