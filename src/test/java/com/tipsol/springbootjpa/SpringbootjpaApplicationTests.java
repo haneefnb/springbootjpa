68f5bd6b40d511781e34effa766aaf7c9a12e1eb
HANEEF NOORBASHA <1000475@infotech.com>
2021-11-21 13:06:38 +0530

    first

---
+++ src/test/java/com/tipsol/springbootjpa/SpringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tipsol.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
