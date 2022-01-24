f32c9afe6133c3b8cd6d019b34b3a39e0b049f3c
in00584 <haneef.noorbasha@azentio.com>
2022-01-24 16:25:40 +0530

    comflicts resolved

--- src/main/java/com/tipsol/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/com/tipsol/springbootjpa/SpringbootjpaApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
