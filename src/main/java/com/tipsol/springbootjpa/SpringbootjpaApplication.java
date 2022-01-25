a2ddbe0c96083ed7fdfcdd0266af94442147143d
in00584 <haneef.noorbasha@azentio.com>
2022-01-25 19:07:58 +0530

    issues fixed

--- src/main/java/com/tipsol/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/com/tipsol/springbootjpa/SpringbootjpaApplication.java
@@ -3,33 +3,39 @@ package com.tipsol.springbootjpa;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * {@summary This is }
+ * 
  * @author 1000475
  */
 @SpringBootApplication
 //@EnableJpaAuditing
 @EnableAsync
-public class SpringbootjpaApplication implements ApplicationRunner {
-	
+public class SpringbootjpaApplication extends SpringBootServletInitializer {
+
 	public static final Logger LOGGER = LoggerFactory.getLogger(SpringbootjpaApplication.class);
 
 	/**
-	 * @author 1000475
-	 * Main Method
+	 * @author 1000475 Main Method
 	 * @param args
 	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(SpringbootjpaApplication.class, args);
 		LOGGER.info("----Started------");
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootjpaApplication.class);
+	}
+
 	/**
 	 * @author 1000475
 	 * @return ModelMapper
@@ -37,14 +43,6 @@ public class SpringbootjpaApplication implements ApplicationRunner {
 	@Bean
 	public ModelMapper getModelMapper() {
 		return new ModelMapper();
-	}
-
-	/**
-	 * @author 1000475
-	 */
-	@Override
-	public void run(final ApplicationArguments args) throws Exception {
-		LOGGER.info("----Application RUnner---------");
 	}
 
 }
